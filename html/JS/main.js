cd5d782219b28f7f2c161123b27836bf916fa529
Taynewbie <tay@Roys-MacBook-Pro.local>
2016-01-19 18:48:12 -0500

    FEWD Final Project

---
+++ html/JS/main.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+    console.log( "ready!" );
+});
+
+  	$(document).ready(function () {
+    $("a").hover(function () {
+        $(this).css("background-color", "#525252");
+    }, function () {
+        $(this).css("background-color", "");
+    });
+});
+
+
+	
